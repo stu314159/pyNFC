813e0d375c89bc4a6ef9eca67936a77c51147e78
Stu Blair <stu314159@gmail.com>
2016-07-13 18:05:24 +0000

    add PartitionAdvisor files; establish pre-processor workflow

---
+++ pyNFC_preprocess.py
@@ -0,0 +1,18 @@
+#pyNFC_preprocess.py
+"""
+   pre-processing steps for pyNFC code.  Basic geometry definition
+   and partitioning information created and input file created prior to
+   parallel simulation run
+
+"""
+
+
+
+
+
+if __name__=='main':
+    """
+      test code 
+    """
+
+
