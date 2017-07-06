e264166df9d33f87adb26f10b1915591a34cd926
Stu Blair <stu314159@gmail.com>
2017-07-06 20:55:04 +0000

    pre-process type

--- pyNFC_preprocess.py
+++ pyNFC_preprocess.py
@@ -33,8 +33,8 @@ sphereChannel.write_mat_file(geom_file_stub)
 
 # set simulation parameters (as used in genInput.py):
 geom_filename = geom_file_stub + '.mat'
-lattice_type = 'D3Q27' # [ 'D3Q15' | 'D3Q19' | 'D3Q27' ]
-partition_style = 'metis' # [ '1D' | '3D' | 'metis']
+lattice_type = 'D3Q19' # [ 'D3Q15' | 'D3Q19' | 'D3Q27' ]
+partition_style = '3D' # [ '1D' | '3D' | 'metis']
 Num_ts = 200
 ts_rep_freq = 1
 Warmup_ts = 0
