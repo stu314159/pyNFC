5caa7233a165b4e91c84aa800e13a6d71ce8cbdd
Stu Blair <stu314159@gmail.com>
2017-07-16 09:22:52 -0500

    added basic tools for incorporation of CPP Lattice classes

---
+++ LBM_DataHandler.cpp
@@ -0,0 +1,28 @@
+/*
+ * LBM_DataHandler.cpp
+ *
+ *  Created on: Jul 10, 2017
+ *      Author: stu
+ */
+
+
+#include "LBM_DataHandler.h"
+
+LBM_DataHandler::LBM_DataHandler(const int numSpd) :
+ux(0),uy(0),uz(0),rho(0),u_bc(0),rho_bc(0),nodeType(0),omega(0),
+piFlat{0,0,0,0,0,0,0,0,0},numSpd(numSpd)
+{
+	f = new float[numSpd];
+	fEq = new float[numSpd];
+	fOut = new float[numSpd];
+
+
+}
+
+LBM_DataHandler::~LBM_DataHandler()
+{
+	delete [] f;
+	delete [] fEq;
+	delete [] fOut;
+
+}
