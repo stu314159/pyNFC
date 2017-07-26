b7e7a9ecc1e7fd1bf756111f65e1196a63cb194c
Stu Blair <stu314159@gmail.com>
2017-07-26 12:55:18 +0000

    added logic to conditionally swap-in INTEL programming environemnt to create metis partitions

--- test_script.sh
+++ test_script.sh
@@ -7,8 +7,22 @@
 # number of OMP threads
 
 # pre-process
+
+# assume going-in that the GNU programming environment
+# is selected.  May need a way to be smarter about this,
+# but the goal is to change to INTEL if 'metis' is the
+# selected partitioning scheme
+
+if [ "$2" = "metis" ]; then
+  module swap PrgEnv-gnu PrgEnv-intel
+fi
 PYTHON_EXE=/p/home/sblair/anaconda2/bin/python
 $PYTHON_EXE ./pyNFC_preprocess.py $1 $2 $3
+
+# switch back to GNU
+if [ "$2" = "metis" ]; then
+  module swap PrgEnv-intel PrgEnv-gnu
+fi
 
 export OMP_NUM_THREADS=$4
 # invoke pyNFC
