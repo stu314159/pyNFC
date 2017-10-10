d40061d65751dd0fa50d73738b77c49900ff20cf
Stu Blair <stu314159@gmail.com>
2017-10-10 15:34:05 -0400

    clean up working directory on workstation

--- gridChan_geom.py
+++ gridChan_geom.py
@@ -44,7 +44,7 @@ gridChan = fc.FluidChannel(Lx_p = aLx_p, Ly_p = aLy_p, \
 
 gridChan.write_mat_file('gridChan')
 
-## add subset object and add to gridChan object
+## create subset objects and add to gridChan object
 ss1 = fc.YZ_Slice(aLx_p/2.,0.15, 0.25, 0.40, 0.80)
 ss2 = fc.YZ_Slice(aLx_p/4.,0.15,0.25,0.40,0.80)
 
