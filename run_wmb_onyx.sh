d71578ce427d6aa98be21ae57dbc17790210d6cd
Stu Blair <stu314159@gmail.com>
2021-07-02 08:22:52 -0500

    clean up work directory on onyx

--- run_wmb_onyx.sh
+++ run_wmb_onyx.sh
@@ -16,13 +16,13 @@
 # saves mat file named ChanCavityTest.mat
 MAT_FILE=wall_mounted_brick.mat
 
-Num_ts=10001
-ts_rep_freq=500
+Num_ts=50001
+ts_rep_freq=5000
 Warmup_ts=0
-plot_freq=500
-Re=125
-dt=0.004
-Cs=5
+plot_freq=2000
+Re=5000
+dt=0.0005
+Cs=50
 Restart_flag=$8
 TimeAvg_flag=$9
 SS_data_flag=0
